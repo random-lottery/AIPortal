781a7c5ec7645d584360abf88c2f1075ad6c552e
random-lottery <ack_online@163.com>
2026-02-11 08:38:36 +0800

    Update auth-function.ts

--- netlify/functions/middleware/auth-function.ts
+++ netlify/functions/middleware/auth-function.ts
@@ -13,14 +13,16 @@ interface AuthResult {
 export const authenticateToken = (event: APIGatewayProxyEvent): AuthResult => {
   const authHeader = event.headers.authorization;
   const token = authHeader && authHeader.split(' ')[1];
+  const supabaseJwtSecret = process.env.SUPABASE_JWT_SECRET;
+  console.log('supabaseJwtSecret', supabaseJwtSecret);
+  console.log('token', token);
 
   if (!token) {
     return { isAuthenticated: false, statusCode: 401, message: 'Unauthorized: No token provided' };
   }
 
   try {
-    const supabaseJwtSecret = process.env.SUPABASE_JWT_SECRET;
-
+   
     if (!supabaseJwtSecret) {
       console.error('SUPABASE_JWT_SECRET is not defined in environment variables.');
       return { isAuthenticated: false, statusCode: 500, message: 'Server configuration error: JWT secret not found.' };
@@ -30,7 +32,7 @@ export const authenticateToken = (event: APIGatewayProxyEvent): AuthResult => {
     const decoded = jwt.verify(token, supabaseJwtSecret, { algorithms: ['HS256'] }) as { sub: string; iat: number; exp: number };
     return { isAuthenticated: true, statusCode: 200, userId: decoded.sub };
   } catch (err: any) { // Cast err to any to access message property safely
-    console.error('Token verification error:', err.message, supabaseJwtSecret, token); // Log the specific error message
+    console.error('Token verification error:', err.name, err.message); // Log the specific error message
     // 针对 invalid algorithm 错误，给出更明确的提示
     if (err.name === 'JsonWebTokenError' && err.message === 'invalid algorithm') {
       return { isAuthenticated: false, statusCode: 403, message: 'Forbidden: Invalid token signature or algorithm mismatch.' };
