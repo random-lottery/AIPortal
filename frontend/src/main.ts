b88b8c0da0c33cb77b8d15aed76a27b64493395b
random-lottery <ack_online@163.com>
2026-03-09 18:52:36 +0800

    update

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,17 +1,24 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { createVuetify } from 'vuetify';
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 import App from './App.vue';
 import router from './router';
 import './assets/main.scss';
+import '@mdi/font/css/materialdesignicons.css';
 
 const vuetify = createVuetify({
   components,
   directives,
   theme: {
     defaultTheme: 'light',
+  },
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: { mdi },
   },
 });
 
